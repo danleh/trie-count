163a0d7d41062e9707069bc04f6eadc5be56b9eb
Daniel Lehmann <mail@dlehmann.eu>
2021-07-15 21:12:56 +0200

    use clap command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,77 @@
-use std::io::Read;
-
 use std::io::{self, BufRead};
+use std::path::PathBuf;
 
 use main_error::MainError;
+use clap::Clap;
 
 use crate::trie::Trie;
 
 mod trie;
 mod unicode_bar_chart;
 
-const BAR_WIDTH: usize = 10;
+const BAR_WIDTH: usize = 20;
+
+#[derive(Clap, Debug)]
+#[clap(
+    author = clap::crate_authors!(),
+    version = clap::crate_version!(),
+    about = clap::crate_description!(),
+)]
+/// Organizes the input lines in a prefix tree (i.e., merging all equal prefixes into a single node)
+/// and counts transitive elements in the tree. Useful
+struct Options {
+    /// Input file to read lines from. [default: stdin]
+    #[clap(short, long, value_name = "file")]
+    input: Option<PathBuf>,
+
+    /// Split only at the given character, e.g., use '/' for splitting paths by directories and file.
+    /// [default: split at every character]
+    #[clap(short, long, value_name = "character")]
+    tokenize_at: Option<char>,
+
+    /// Sort the trie nodes by number of contained elements, i.e., largest subtrees come first.
+    /// [default: true]
+    #[clap(short, long)]
+    sort_by_count: bool,
+
+    /// Show a bar of percent next to the count. [default: false]
+    #[clap(short, long)]
+    bar: bool,
+}
 
 fn main() -> Result<(), MainError> {
     // test_order();
 
-    let mut trie = Trie::new();
+    let options = Options::parse();
+    // println!("{:?}", options);
+
+    let mut trie = if let Some(token) = options.tokenize_at {
+        Trie::with_split_token(token)
+    } else  {
+        Trie::new()
+    };
 
     let stdin  = io::stdin();
     for line in stdin.lock().lines() {
         trie.insert(&line?);
     }
 
-    trie.sort_by_count();
+    if options.sort_by_count {
+        trie.sort_by_count();
+    }
 
-    // TODO align sizes
-    // let max_size_width = trie.len().to_string().len();
-
-    let total = trie.len();
+    let max_size_width = trie.len().to_string().len();
+    let total_inv = 1.0 / trie.len() as f64;
 
     for (prefix, level, count) in trie.by_levels_with_count() {
         let indent = "  ".repeat(level);
-        let bar = unicode_bar_chart::bar_str(count as f64 / total as f64, BAR_WIDTH);
-        println!("{:<15} {} {}", format!("{}{}", indent, count), bar, prefix);
+        print!("{}{:<width$} ", indent, count, width=max_size_width);
+        if options.bar {
+            let total_fraction = count as f64 * total_inv;
+            let bar = unicode_bar_chart::unicode_bar_str(total_fraction, BAR_WIDTH);
+            print!("{} ", bar);
+        }
+        println!("{}", prefix);
     }
 
     Ok(())

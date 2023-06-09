27cae020bd7063a2804afa551ba3c8f2ad7177ae
Daniel Lehmann <mail@dlehmann.eu>
2023-06-09 18:09:29 +0200

    make integration test slightly stricter

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -18,8 +18,8 @@ fn testcase<const N: usize>(name: &str, options: [&str; N]) {
         .arg(format!("tests/inputs/{name}.txt"))
         .output()
         .unwrap();
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let stderr = String::from_utf8_lossy(&output.stderr);
+    let stdout = String::from_utf8(&output.stdout).unwrap();
+    let stderr = String::from_utf8(&output.stderr).unwrap();
     assert!(
         output.status.success(),
         "\nstdout:\n{stdout}stderr:\n{stderr}"
